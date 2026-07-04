06df969ae3ea65fb911682305fc202844d80c22e
Sakshi Verma <sakshiverma9335@gmail.com>
2026-07-05 00:42:16 +0530

    Added Ward model and admin configuration

--- wards/admin.py
+++ wards/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Ward
 
-# Register your models here.
+
+@admin.register(Ward)
+class WardAdmin(admin.ModelAdmin):
+    list_display = (
+        "ward_name",
+        "ward_type",
+        "floor",
+        "capacity",
+        "status",
+    )
+
+    list_filter = ("ward_type", "status")
+    search_fields = ("ward_name",)
