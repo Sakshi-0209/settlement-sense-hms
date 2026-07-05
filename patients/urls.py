c0eba43b29cc6e489b7eecfbfeeb2c74604f14f8
Sakshi Verma <sakshiverma9335@gmail.com>
2026-07-06 02:00:03 +0530

    Implement patient listing and registration workflow

--- patients/urls.py
+++ patients/urls.py
@@ -2,5 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+
     path("", views.patient_list, name="patients"),
+
+    path(
+        "add/",
+        views.add_patient,
+        name="add_patient",
+    ),
+
 ]
--- patients/views.py
+++ patients/views.py
@@ -1,5 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Patient
+from .forms import PatientForm
 
 
 def patient_list(request):
-    return render(request, "patients/patient_list.html")
+
+    patients = Patient.objects.all()
+
+    return render(
+        request,
+        "patients/patient_list.html",
+        {"patients": patients},
+    )
+
+
+def add_patient(request):
+
+    if request.method == "POST":
+
+        form = PatientForm(request.POST, request.FILES)
+
+        if form.is_valid():
+
+            form.save()
+
+            return redirect("patients")
+
+    else:
+
+        form = PatientForm()
+
+    return render(
+        request,
+        "patients/add_patient.html",
+        {"form": form},
+    )
